ebae7da7f27ec1aeda4146c42d57dbbc672b15b3
yunsehwan <sh10302@naver.com>
2022-08-25 15:04:34 +0900

    apply new logo

--- src/components/pages/main/index.tsx
+++ src/components/pages/main/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import textStyle from "../../../style/textStyle";
 
 const Main = () => {
     return (
-        <View>
-            <Text style={textStyle.headline1}>Hello!</Text>
-        </View>
+        <SafeAreaView>
+            <View>
+                <Text style={textStyle.headline1}>Hello!</Text>
+            </View>
+        </SafeAreaView>
     )
 }
 
-export {Main}
+export { Main }
