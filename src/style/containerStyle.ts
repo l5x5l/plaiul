4a0ed4fa9a33ba8148e9812b9d97d8d2d4e18f19
yunsehwan <sh10302@naver.com>
2022-08-26 10:43:10 +0900

    메인 화면 구성 세팅

---
+++ src/style/containerStyle.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const ContainerStyle = StyleSheet.create({
+    container : {
+        flex : 1
+    },
+    toolbar : {
+        flexDirection : "row",
+        height : 56
+    }
+})
+
+export default ContainerStyle
