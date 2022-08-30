765fce817df496d098396a66985133352bd4004b
yunsehwan <sh10302@naver.com>
2022-08-30 14:35:51 +0900

    커뮤니티 화면 구성

---
+++ src/components/blocks/postView/index.tsx
@@ -0,0 +1,47 @@
+import { useTheme } from "@react-navigation/native";
+import React, { useMemo } from "react";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import textStyle from "../../../style/textStyle";
+import { PostDto } from "../../../type/DTO/postDto";
+
+export declare type PostViewProps = {
+    post: PostDto,
+    onClick?: (idx: number) => void
+}
+
+const PostView = (props: PostViewProps) => {
+    const { colors } = useTheme()
+
+    return (
+        useMemo(() =>
+            <Pressable style={{ flex: 1, marginHorizontal : 2, marginTop : 16, borderColor : colors.border, borderWidth : 2 }} onPress={() => {
+
+            }}>
+                <View style={PostViewStyle.container}>
+                    <Image source={{ uri: props.post.thumbnail }} style={PostViewStyle.thumbNail} />
+                    <View style={PostViewStyle.textArea}>
+                        <Text style={[textStyle.title2, { color: colors.text }]} numberOfLines={1}>{props.post.title}</Text>
+                        <Text style={[textStyle.body3, { color: colors.text, marginTop: 8 }]} numberOfLines={2}>{props.post.content}</Text>
+                        <Text style={[textStyle.body3, { color: colors.text, marginTop: 8 }]} numberOfLines={1}>{props.post.user?.nickname}</Text>
+                    </View>
+                </View>
+            </Pressable>
+            , [])
+    )
+}
+
+const PostViewStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    thumbNail: {
+        width: "100%",
+        aspectRatio: 1
+    },
+    textArea: {
+        padding: 8,
+        flex: 1
+    }
+})
+
+export { PostView }
--- src/components/pages/main/community/index.tsx
+++ src/components/pages/main/community/index.tsx
@@ -1,12 +1,130 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { useTheme } from "@react-navigation/native";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
+import textStyle from "../../../../style/textStyle";
+import { PostDto } from "../../../../type/DTO/postDto";
+import { Line } from "../../../atoms/line";
+import { SelectButton } from "../../../atoms/selectButton";
+import { PostView } from "../../../blocks/postView";
 
 const CommunityScreen = () => {
+
+    const { colors } = useTheme()
+    const [selectedCategory, setSelectedCategory] = useState<"story" | "qna">("story")
+    const [postList, setPostList] = useState<PostDto[]>([])
+    const pageIdx = useRef(0);
+
+    function getDatas(postIdx : number) : PostDto[] {
+        return [
+            {
+                storyIdx : postIdx,
+                content : "위 사진은 react 아이콘이에요",
+                title : `${postIdx} 스토리입니다.`,
+                thumbnail : "https://reactnative.dev/img/tiny_logo.png",
+                user : {
+                    userIdx : 0,
+                    nickname : "probe"
+                }
+            },
+            {
+                storyIdx : postIdx + 1,
+                content : "위 사진은 react 아이콘이에요",
+                title : `${postIdx} 스토리입니다.`,
+                thumbnail : "https://reactnative.dev/img/tiny_logo.png",
+                user : {
+                    userIdx : 0,
+                    nickname : "probe"
+                }
+            },
+            {
+                qnaIdx : postIdx + 2,
+                content : "위 사진은 react 아이콘이에요 이건 조금만 더 길게 적어볼께요. 여기서 더 적으면....?",
+                title : `${postIdx} Qna입니다.`,
+                thumbnail : "https://reactnative.dev/img/tiny_logo.png",
+                user : {
+                    userIdx : 0,
+                    nickname : "probe"
+                }
+            },
+            {
+                qnaIdx : postIdx + 3,
+                content : "위 사진은 react 아이콘이에요",
+                title : `${postIdx} Qna입니다.`,
+                thumbnail : "https://reactnative.dev/img/tiny_logo.png",
+                user : {
+                    userIdx : 0,
+                    nickname : "probe"
+                }
+            },
+            {
+                storyIdx : postIdx + 4,
+                content : "위 사진은 react 아이콘이에요",
+                title : `${postIdx} 스토리입니다.`,
+                thumbnail : "https://reactnative.dev/img/tiny_logo.png",
+                user : {
+                    userIdx : 0,
+                    nickname : "probe"
+                }
+            },
+            {
+                qnaIdx : postIdx + 5,
+                content : "위 사진은 react 아이콘이에요",
+                title : `${postIdx} Qna입니다.`,
+                thumbnail : "https://reactnative.dev/img/tiny_logo.png",
+                user : {
+                    userIdx : 0,
+                    nickname : "probe"
+                }
+            }
+        ]
+    }
+    
+    function setData() {
+        pageIdx.current = pageIdx.current + 1
+        const response = getDatas(pageIdx.current)
+        setPostList([...postList, ...response])
+    }
+    useEffect ( () => {
+        setData()
+    }
+    , [])
+
     return (
-        <View style={{flex : 1}}>
-            <Text>Community</Text>
+        <View style={{ flex: 1, paddingHorizontal: 16 }}>
+            <Text style={[textStyle.headline2, { color: colors.text }]}>Community</Text>
+            {
+                useMemo(() =>
+                    <View style={CommunityStyle.categoryButtonArea}>
+                        <SelectButton isSelected={selectedCategory === "story"} text={"story"} marginStart={0} onPress={function (): void {
+                            setSelectedCategory("story")
+                        }} />
+                        <SelectButton isSelected={selectedCategory === "qna"} text={"qna"} marginStart={8} onPress={function (): void {
+                            setSelectedCategory("qna")
+                        }} />
+                    </View>
+                    , [selectedCategory])
+            }
+            <View style={{ flexDirection: "row", justifyContent: "space-between", marginTop: 24, alignItems: "center" }}>
+                <Text style={[textStyle.body2, { color: colors.text }]}>{`총 검색결과 : ${6}개`}</Text>
+                <Pressable>
+                    <View style={{ padding: 8 }}>
+                        <Text style={[textStyle.body3, { color: colors.text }]}>최신순</Text>
+                    </View>
+                </Pressable>
+            </View>
+            <Line marginTop={8} />
+            <FlatList numColumns={2} data={postList} renderItem={({ item }) => <PostView post={item} />}
+                onEndReachedThreshold={0.8}
+                onEndReached={() => { setData()}} />
         </View>
     )
 }
 
-export {CommunityScreen}
+const CommunityStyle = StyleSheet.create({
+    categoryButtonArea: {
+        flexDirection: "row",
+        marginTop: 24
+    }
+})
+
+export { CommunityScreen }
