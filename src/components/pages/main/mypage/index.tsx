817c0595caba1115fe051ca9befdeb3d75ce40e2
yunsehwan <sh10302@naver.com>
2022-08-30 14:34:18 +0900

    마이페이지 화면 구성

--- src/components/pages/main/mypage/index.tsx
+++ src/components/pages/main/mypage/index.tsx
@@ -1,12 +1,69 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { useTheme } from "@react-navigation/native";
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import textStyle from "../../../../style/textStyle";
+import { UserDto } from "../../../../type/DTO/userDto";
+import { Line } from "../../../atoms/line";
+import { TextButton } from "../../../atoms/textButton";
 
 const MyPageScreen = () => {
+
+    const { colors } = useTheme();
+    const [userInfo, setUserInfo] = useState<UserDto>({ nickname: "probe", userIdx: -1 })
+
     return (
-        <View>
-            <Text>My Page</Text>
-        </View>
+        <ScrollView style={MyPageStyle.container}>
+            <View style={MyPageStyle.titleArea}>
+                <Text style={[textStyle.headline1, { color: colors.text, flex: 1 }]}>{(userInfo.nickname !== "loading") ? `반가워요\n${userInfo.nickname}님!` : `loading...`}</Text>
+                <Image style={{ height: 64, width: 64, borderRadius: 32 }} source={{ uri: (userInfo.profile !== undefined) ? userInfo.profile : "https://reactnative.dev/img/tiny_logo.png" }} />
+            </View>
+            <Line marginTop={24} />
+            <Text style={[textStyle.headline3, { color: colors.text, marginTop: 24, marginBottom: 16 }]}>내 활동</Text>
+            <TextButton text={"좋아요 한 게시글"} onPress={function (): void {
+
+            }} />
+            <TextButton text={"좋아요 한 Grower's tip"} onPress={function (): void {
+
+            }} />
+            <TextButton text={"내가 쓴 게시글"} onPress={function (): void {
+
+            }} />
+            <TextButton text={"내가 쓴 Grower's tip"} onPress={function (): void {
+
+            }} />
+            <TextButton text={"댓글 단 게시글"} onPress={function (): void {
+
+            }} />
+            <Line marginTop={16} />
+            <Text style={[textStyle.headline3, { color: colors.text, marginTop: 24, marginBottom: 16 }]}>설정</Text>
+            <TextButton text={"내 계정 정보"} onPress={function (): void {
+
+            }} />
+            <TextButton text={"비밀번호 변경"} onPress={function (): void {
+
+            }} />
+            <TextButton text={"프로필 수정"} onPress={function (): void {
+
+            }} />
+            <TextButton text={"고객센터"} onPress={function (): void {
+
+            }} />
+            <Line marginTop={16} />
+            <View style={{ height: 56 }} />
+        </ScrollView>
     )
 }
 
-export {MyPageScreen}
+const MyPageStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 16
+    },
+    titleArea: {
+        flexDirection: "row",
+        marginTop: 24
+    },
+})
+
+export { MyPageScreen }
--- src/type/DTO/userDto.ts
+++ src/type/DTO/userDto.ts
@@ -1,4 +1,5 @@
 export declare type UserDto = {
     userIdx : number,
-    nickname : string
+    nickname : string,
+    profile? : string,
 }
