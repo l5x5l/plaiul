da51dd6aed8b9c50bac06a8c1e2049fb6b5b3ee8
yunsehwan <sh10302@naver.com>
2022-09-15 12:35:21 +0900

    [수정] apiBaseRespone 형식 수정 및 mapping함수 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,37 +1,59 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosResponse } from "axios";
 
 const baseUri = "http://15.164.214.109"
 
-const errorCode = [1000, 1100, 1101, 1102, 1103, 1104, 1200, 2100, 2101, 2102, 2103, 2104, 3000, 3001, 3002, 3003, 3110, 3111, 4000]
-
-export const getStory = (id : number) => axios.get(`${baseUri}/api/stories/${id}`).then(data => data.data).catch((error) => {
-    console.log (error.response.data)
+// 스토리 상세 조회
+export const getStory = (id: number) => axios.get(`${baseUri}/api/stories/${id}`).then(data => data.data).catch((error) => {
+    console.log(error.response.data)
     if (error.response?.data !== undefined)
         return error.response.data
     else
         throw error
 })
 
-export const getStoryList = (sort : string, cursor ?: string) => axios.get(`${baseUri}/api/stories`, {params:{sort : sort, cursor : cursor}, headers:{}}).then(data => {
+// 스토리 리스트 조회
+export const getStoryList = (sort: string, cursor?: string) => axios.get(`${baseUri}/api/stories`, { params: { sort: sort, cursor: cursor }, headers: {} }).then(data => {
     return data.data
 }).catch((error) => {
     if (error.response?.data !== undefined)
         return error.response.data
-    else 
+    else
         throw error
 })
 
-export const getQnaList = (sort : string, cursor ?: string) => axios.get(`${baseUri}/api/stories`, {params:{sort : sort, cursor : cursor}, headers:{}}).catch((error) => {
+// qna 리스트 조회
+export const getQnaList = (sort: string, cursor?: string) => axios.get(`${baseUri}/api/stories`, { params: { sort: sort, cursor: cursor }, headers: {} }).catch((error) => {
     if (error.response?.data !== undefined)
         return error.response.data
-    else 
+    else
         throw error
 })
 
+
+
+// 기본 요소들
 export type apiBaseResponse<T, G> = {
-    code ?: number,
-    message ?: string,
-    erros ?: string,
-    data : T,
-    meta : G
+    code?: number,
+    message?: string,
+    erros?: string,
+    data?: T,
+    meta?: G,
+    errors?: string[]
+}
+
+export function toApiBaseResponse<T, G>(response: AxiosResponse<any, any>) {
+    return {
+        data: response.data.data
+    } as apiBaseResponse<T, G>
+}
+
+export function toApiErrorResponse<T, G>(error: any) {
+    if (error.response?.data !== undefined)
+        return {
+            code: error.response.data.code,
+            message: error.response.data.message,
+            errors: error.response.data.errors
+        } as apiBaseResponse<T, undefined>
+    else
+        throw error
 }
