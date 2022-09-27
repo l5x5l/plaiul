dd73def6ecb05857d23fb2a6d87d58c1597ed815
yunsehwan <sh10302@naver.com>
2022-09-27 16:42:29 +0900

    [수정] 체크박스의 클릭 이벤트 인자를 선택적 인자로 변경

--- src/components/atoms/checkbox/index.tsx
+++ src/components/atoms/checkbox/index.tsx
@@ -5,23 +5,30 @@ import { Image, Pressable } from "react-native";
 export declare type CheckBoxProps = {
     style: "stroke" | "fill",
     isChecked: boolean,
-    setIsChecked: (isChecked: boolean) => void
+    setIsChecked?: (isChecked: boolean) => void
 }
 
 const CheckBox = (props: CheckBoxProps) => {
 
-    const {colors} = useTheme();
+    const { colors } = useTheme();
 
     return (
-        <Pressable onPress={() => {
-            props.setIsChecked(!props.isChecked)
-        }}>
+        props.setIsChecked ?
+            <Pressable onPress={() => {
+                props.setIsChecked!!(!props.isChecked)
+            }}>
+                <Image source={
+                    props.style === "fill" ?
+                        require("../../../assets/images/checkbox_fill_40.png")
+                        : require("../../../assets/images/checkbox_stroke_40.png")}
+                    style={{ height: 30, width: 30, tintColor: props.isChecked ? colors.border : colors.card }} />
+            </Pressable> 
+            :
             <Image source={
-                props.style==="fill" ?
-                     require("../../../assets/images/checkbox_fill_40.png")
-                    : require("../../../assets/images/checkbox_stroke_40.png")} 
-                    style={{ height: 30, width: 30, tintColor : props.isChecked ? colors.border : colors.card }} />
-        </Pressable>
+                props.style === "fill" ?
+                    require("../../../assets/images/checkbox_fill_40.png")
+                    : require("../../../assets/images/checkbox_stroke_40.png")}
+                style={{ height: 30, width: 30, tintColor: props.isChecked ? colors.border : colors.card }} />
     )
 
 }
